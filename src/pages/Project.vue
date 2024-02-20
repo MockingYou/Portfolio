908d5ec12547256a190ab48030e1d1d0369f79d5
Vlad <vlad.mocanu13@gmail.com>
2024-02-20 15:34:58 +0200

    - css changes

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="portfolio-item">    
-        <div class="image" @mouseover="colorizeImage" @mouseleave="resetImage">
-            <img :src="image" alt="" :style="{ filter: isColorized ? 'grayscale(0%)' : 'grayscale(100%)' }" />
-        </div>
-        <div class="hover-items">
-            <div v-text="title" />
-            <div class="project-desc" v-text="description" />
-            <ProjectButton :link="link" :icon="icon" /> 
-        </div>
+    <div class="portfolio-item">
+      <div class="image" @mouseover="colorizeImage" @mouseleave="resetImage">
+        <img :src="image" alt="" :style="{ filter: isColorized ? 'grayscale(0%)' : 'grayscale(100%)' }" />
+      </div>
+      <div class="hover-items" @mouseover="colorizeImage" @mouseleave="resetImage" :style="{ opacity: 0.95 }">
+        <div v-text="title" />
+        <div class="project-desc" v-text="description" />
+        <ProjectButton :link="link" :icon="icon" />
+      </div>
     </div>
     <span v-for="(language, key, index) in languages" :key="key">
-        <img :src="language" :alt="key" v-if="language.includes('/img/')" :style="{ paddingTop: '5px', width: '30px', height: '30px', color: 'white' }" />
-        <v-icon v-else>{{ language}}</v-icon>
-        <template v-if="index !== Object.keys(languages).length - 1">, </template>
+      <img :src="language" :alt="key" v-if="language.includes('/img/')" :style="{ paddingTop: '5px', width: '30px', height: '30px', color: 'white' }" />
+      <v-icon v-else>{{ language }}</v-icon>
+      <template v-if="index !== Object.keys(languages).length - 1">, </template>
     </span>
-</template>
+  </template>
+  
 
 <script>
 import ProjectButton from './ProjectButton.vue';
