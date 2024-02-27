71f6ee11de59cdc37968f637a9d5be09adaf00a6
Vlad <vlad.mocanu13@gmail.com>
2024-02-27 16:54:24 +0200

    - project details page update

--- src/pages/Projects/ProjectsDetails.vue
+++ src/pages/Projects/ProjectsDetails.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="body">
-		<h2>Project Details</h2>
-		<Spinner v-if="loading"/>
+        <div class="history">
+            <button class="back-button" @click="goBack()">Projects</button>
+            <v-icon class="chevron">mdi-chevron-right</v-icon>
+            <p class="history-title">{{ projectData.title }}</p>
+        </div>
+        <h2>Project Details</h2>
+        <Spinner v-if="loading"/>
         <div v-else>
-			<div class="image" >
-				<img :src="projectData.imageName" alt=""  />
-			</div>
+            <div class="image">
+                <img :src="projectData.imageName" alt=""/>
+            </div>
             <h3>{{ projectData.title }}</h3>
             <p>{{ projectData.description }}</p>
         </div>
@@ -14,12 +19,13 @@
 
 <script>
 import Spinner from '../../components/ui/Spinner.vue';
+import router from '../../router/router'
 import axios from 'axios';
 
 export default {
     props: {
-		id: String
-	},
+        id: String
+    },
     data() {
         return {
             loading: true,
@@ -33,12 +39,15 @@ export default {
         async fetchProjectDetails() {
             try {
                 const response = await axios.get(`https://portfolio-service-1j6y.onrender.com/api/Project/${this.id}`);
-				this.projectData = response.data.data;
+                this.projectData = response.data.data;
             } catch (error) {
                 console.error('Error fetching project:', error);
             } finally {
                 this.loading = false;
             }
+        },
+        goBack() {
+            router.push({path: '/projects'})
         }
     },
     mounted() {
@@ -49,15 +58,39 @@ export default {
 
 <style scoped>
 .body {
-	position: absolute;
+    position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    text-align: center; 
+    text-align: center;
 }
+
 .image img {
-	width: 100%;
-  	height: 300px;
-  	border-radius: 15px;
+    width: 100%;
+    height: 300px;
+    border-radius: 15px;
+}
+
+.back-button {
+    color: white;
+    cursor: pointer;
+    margin-right: 10px;
+}
+
+.history {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 20px;
+}
+
+.history p {
+    color: #039BE5;
+    padding: 10px;
+    font-weight: bold;
+}
+
+.chevron {
+    color: white;
 }
 </style>
