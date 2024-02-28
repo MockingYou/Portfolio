8ffa28e45d0fd6efa2784b3a128f58ed5b60b1d0
Vlad <vlad.mocanu13@gmail.com>
2024-02-28 12:54:46 +0200

    - updated ImageSlider with different type

--- src/components/ImageSlider.vue
+++ src/components/ImageSlider.vue
@@ -1,19 +1,33 @@
 <template>
-	<swiper :navigation="true" :modules="modules" class="mySwiper">
+	<swiper 
+		effect="cube" 
+		class="mySwiper" 
+		:modules="modules" 
+		:loop="true" 
+		:grabCursor="true"
+		:pagination="true"
+		:cubeEffect="{
+			shadow: true,
+			slideShadow: true,
+			shadowOffset: 20,
+			shadowScale: 0.94,
+		}"
+	>
 	  	<swiper-slide>
 			<img :src="image" alt=""/>
 		</swiper-slide>
-	  	<swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
-	  	<swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
-	  	<swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
-	  	<swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+	  	<swiper-slide>Slide 3</swiper-slide>
+	  	<swiper-slide>Slide 5</swiper-slide>
+	  	<swiper-slide>Slide 7</swiper-slide>
+
 	</swiper>
 </template>
 <script>
 	import { Swiper, SwiperSlide } from 'swiper/vue';
+	import { EffectCube, Pagination } from 'swiper/modules';
 	import 'swiper/css';
-	import 'swiper/css/navigation';
-	import { Navigation } from 'swiper/modules';
+	import 'swiper/css/effect-cube'
+	import 'swiper/css/pagination'
   
 	export default {
 		props: {
@@ -25,7 +39,7 @@
 	  	},
 	  	setup() {
 			return {
-		  		modules: [Navigation],
+		  		modules: [EffectCube, Pagination],
 			};
 	  	},
 	};
@@ -35,16 +49,17 @@
 .swiper {
   width: 100%;
   height: 100%;
+  overflow: visible;
 }
-.swiper-slide {
+/* .swiper-slide {
   text-align: center;
   font-size: 18px;
   display: flex;
   justify-content: center;
   align-items: center;
-}
+} */
 .swiper-slide img {
-  display: block;
+  /* display: block; */
   width: 100%;
   height: 100%;
   object-fit: cover;
--- src/pages/Projects/ProjectsDetails.vue
+++ src/pages/Projects/ProjectsDetails.vue
@@ -5,59 +5,87 @@
             <v-icon class="chevron">mdi-chevron-right</v-icon>
             <p class="history-title">{{ projectData.title }}</p>
         </div>
-        <Spinner v-if="loading"/>
+        <Spinner v-if="loading" />
         <div class="project-data" v-else>
             <div class="slider">
                 <ImageSlider :image="projectData.imageName" />
             </div>
-            <h3>{{ projectData.title }}</h3>
+            <h2>{{ projectData.title }}</h2>
             <p>{{ projectData.description }}</p>
-            <a :href="projectData.link" target="_blank">
-                <v-icon>{{ projectData.icon }}</v-icon>
-                <p>{{projectData.link}}</p>
-            </a>
+            <div class="details-panel">
+                <div class="details">
+                    <h3> <v-icon class="title-icon">mdi mdi-devices</v-icon> Made for</h3>    
+                </div>
+                <div class="details">
+                    <h3> <v-icon class="title-icon">mdi mdi-file-code</v-icon> Built with </h3>
+                    <span>
+                        <p>{{ parsedLanguages }}</p>
+                    </span>
+                </div>
+                <div class="details">
+                    <h3> <v-icon class="title-icon">mdi-git</v-icon> Source code</h3>
+                </div>
+                <div class="details">
+                    <h3> <v-icon class="title-icon">mdi-open-in-new</v-icon> Deployed on</h3>
+                    <a :href="projectData.link" target="_blank" class="project-link">
+                        <p>{{ projectData.link }}</p>
+                    </a>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import Spinner from '../../components/ui/Spinner.vue';
-import ImageSlider from '../../components/ImageSlider.vue'
-import router from '../../router/router'
-import axios from 'axios';
+import Spinner from "../../components/ui/Spinner.vue";
+import ImageSlider from "../../components/ImageSlider.vue";
+import router from "../../router/router";
+import axios from "axios";
 
 export default {
     props: {
-        id: String
+        id: String,
     },
     data() {
         return {
             loading: true,
-            projectData: {}
+            projectData: {},
+            parsedLanguages: "",
         };
     },
     components: {
         Spinner,
-        ImageSlider
+        ImageSlider,
     },
     methods: {
         async fetchProjectDetails() {
             try {
-                const response = await axios.get(`https://portfolio-service-1j6y.onrender.com/api/Project/${this.id}`);
+                const response = await axios.get(
+                    `https://portfolio-service-1j6y.onrender.com/api/Project/${this.id}`
+                );
                 this.projectData = response.data.data;
+                this.parseLanguages();
             } catch (error) {
-                console.error('Error fetching project:', error);
+                console.error("Error fetching project:", error);
             } finally {
                 this.loading = false;
             }
         },
         goBack() {
-            router.push({path: '/projects'})
-        }
+            router.push({ path: "/projects" });
+        },
+        parseLanguages() {
+            try {
+                let prasedJson = JSON.parse(this.projectData.languages);
+                this.parsedLanguages = Object.keys(prasedJson).join(", ")
+            } catch (error) {
+                console.error("Error parsing languages:", error);
+            }
+        },
     },
     mounted() {
         this.fetchProjectDetails();
-    }
+    },
 };
 </script>
 
@@ -68,14 +96,18 @@ export default {
     left: 50%;
     transform: translate(-50%, -50%);
     text-align: center;
+    justify-content: center;
+    align-items: center;
+    justify-items: center;
+    margin-top: 100px;
 }
 
 .slider {
-    display: flex;
     align-items: center;
     justify-items: center;
-    width: 800px;
-    height: 500px;
+    width: 600px;
+    height: 600px;
+    margin-bottom: 50px;
 }
 .image img {
     width: 100%;
@@ -97,7 +129,7 @@ export default {
 }
 
 .history p {
-    color: #039BE5;
+    color: #039be5;
     padding: 10px;
     font-weight: bold;
 }
@@ -107,8 +139,43 @@ export default {
 }
 
 .project-data {
-    align-items: center;
+    display: flex;
+    flex-direction: column; 
+    align-items: center; 
+    justify-content: center; 
+    text-align: center;
+}
+
+.project-data p {
+    margin-bottom: 50px;
+}
+.details-panel {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 20px;
+    justify-items: center;
+}
+.project-link {
+    text-decoration: none;
+}
+.project-link:hover,
+.project-link:focus ,
+.project-link:active {
+    background-color: unset !important;
+    text-decoration: none !important;
+    outline: none !important;
+    color:#039be5;
+}
+.title-icon {
+    color:#039be5;
+    font-size: 30px;
+    margin-right: 10px;
+}
+.details {
     justify-content: center;
-    margin-bottom: 20px;
+    text-align: center;
+    justify-items: center;
+    align-items: center; 
+
 }
 </style>
